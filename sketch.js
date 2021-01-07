eed8e50b4e4ae8484616a30c518fe08e2db1de67
Noah Viernes <visiblepolis@MacBook-Pro-4.local>
2021-01-07 22:48:32 +0900

    add sketch.js + delete partial menu

--- sketch.js
+++ sketch.js
@@ -1,54 +1,20 @@
-var canvas;
-let a = 80; // Create a global variable "a"
+var x = [];
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-	canvas.position(350, 0);
-	canvas.style('z-index', '-1');
-
-  background(255);
-  stroke(1);
-  noLoop();
+createCanvas(1040, 100);
+noStroke();
+fill(255, 200);
+for (var i = 0; i < 3000; i++) {
+x[i] = random(-1000, 200);
+}
 }
 
 function draw() {
-  // Draw a line using the global variable "a"
-  line(a, 0, a, height);
-<<<<<<< HEAD
-	stroke(0, 0, 0, 55);
-
-  // Use a local variable a in for loop
-  for (let a = 20; a < 200; a += 4) {
-=======
-	stroke(255, 0, 0);
-
-  // Use a local variable a in for loop
-  for (let a = 120; a < 200; a += 3) {
->>>>>>> f466d809b07a63fc71e5b57a8ba1b9662b7f36f0
-    line(a, 0, a, height);
-  }
-
-  // Make a call to the custom function drawAnotherLine()
-  drawAnotherLine();
-
-  // Make a call to the custom function drawYetAnotherLine()
-  drawYetAnotherLine();
+background(0);
+for (var i = 0; i < x.length; i++) {
+x[i] += 0.7;
+var y = i * 0.133;
+arc(x[i], y, 12, 10, 0.72, 5.76);
 }
-
-function drawAnotherLine() {
-  // Create a new variable "a" local to this function
-  let a = 320;
-  // Draw a line using the local variable "a"
-  line(a, 0, a, height);
 }
-
-function drawYetAnotherLine() {
-  // Because no new local variable "a" is set,
-  // this line draws using the original global
-  // variable "a" which is set to the value 20.
-<<<<<<< HEAD
-  line(a + 13, 0, a + 13, height);
-=======
-  line(a + 3, 0, a + 3, height);
->>>>>>> f466d809b07a63fc71e5b57a8ba1b9662b7f36f0
-}
+//Slightly modified from Example 11-3 in "Getting Started with p5.js"
